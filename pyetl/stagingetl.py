776fcf492602a41ea615b7a854e9f356dbfc94b8
febridev <febriyantabidin@gmail.com>
2021-11-20 23:07:04 +0700

    get table teomjobgrade

--- pyetl/stagingetl.py
+++ pyetl/stagingetl.py
@@ -6,20 +6,36 @@ from dotenv import load_dotenv
 #load .env file
 load_dotenv()
 
-#Connection String dbdemo
-db = mysql.connector.connect(
+# #Connection String dbdemo
+dbcondemo = mysql.connector.connect(
     host=os.getenv('hostdbdemo'),
     port=os.getenv('portdbdemo'),
     user=os.getenv('userdbdemo'),
     passwd=os.getenv('passdbdemo')
 )
 
-if db.is_connected():
-    print('Berhasil')
+# Check Connection Is Open
+# if dbcondemo.is_connected():
+#     print('Berhasil')
 
-#Close Connection
-db.close()
+# #Close Connection
+# dbcondemo.close()
 
-if db.is_connected() == False:
-    print('Close')
+#Check Connection Is Closed
+# if dbcondemo.is_connected() == False:
+#     print('Close')
 
+
+#Function Get Data From DB Demo Table 
+def get_teomjobgrade():
+    cursor = dbcondemo.cursor()
+    sqltext = "select * from dbdemo.teomjobgrade;"
+    cursor.execute(sqltext)
+    result = cursor.fetchall()
+    dbcondemo.close()
+    return result
+
+if __name__ == '__main__':
+    for data in get_teomjobgrade():
+        print(data[0])
+    
